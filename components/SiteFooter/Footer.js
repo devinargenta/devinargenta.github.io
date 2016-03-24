08aedc763663f9a85caddfff8cf2d5c1fb08bf3c
Devin Argenta <devin@devinargenta.com>
2016-03-24 18:19:47 -0400

    added the footer moule forreal

---
+++ components/SiteFooter/Footer.js
@@ -0,0 +1,25 @@
+
+/**
+ * React Static Boilerplate
+ * https://github.com/koistya/react-static-boilerplate
+ * Copyright (c) Konstantin Tarkus (@koistya) | MIT license
+ */
+
+import React from 'react';
+import '../Layout/Layout.scss';
+function Footer( ) {
+  return (
+    <footer>
+      <div>
+        Built With React <br/>
+      Boilerplate:
+      <a href="https://github.com/koistya/react-static-boilerplate">
+        https://github.com/koistya/react-static-boilerplate
+      </a>
+      </div>
+    </footer>
+
+  );
+}
+
+export default Footer;
